22b92340d33cb392e2efb7565dd8efcc5160669d
Ramon Silva <63071007+ramonsilva2@users.noreply.github.com>
2021-02-21 11:21:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 const btn = document.getElementById("btn");
 const ipt = document.getElementById("ipt");
-var vetorNum = [];
-var result;
+const rst = document.getElementById("result");
+
+
 
 btn.addEventListener("click", () => {
+    let vetorNum = [];
+    let vetorSignal = [];
+    let contNum = 0;
+    let contSig = 0;
+    let result;
 
-    ipt.value.forEach(n => {
-        if(ipt.value !== '-' || ipt.value !== '+') {
-            vetorNum = n;
+    for(let i=0;i<ipt.value.length;i++){
+        if(ipt.value[i] == "-" || ipt.value[i] == "+") {
+            vetorSignal[contSig] = ipt.value[i];
+            contSig++;
         }
-        console.log(vetorNum);
-    })
+    }
+    for(let i=0;i<ipt.value.length;i++){
+        if(ipt.value[i] == "-" || ipt.value[i] == "+") {
+
+        } else {
+            vetorNum[contNum] = ipt.value[i];
+            contNum++;
+        }
+    }
+    result = parseInt(vetorNum[0]);
+    for(let i=0;i<vetorSignal.length;i++){
+        if(vetorSignal[i]=='+') {
+            result += parseInt(vetorNum[i+1]);
+        }
+    }
+    for(let i=0;i<vetorSignal.length;i++){
+        if(vetorSignal[i]=='-') {
+            result -= parseInt(vetorNum[i+1]);
+        }
+    }
+    rst.innerHTML = `<h3>${result}</h3>`
+
 });
